fd78e89093fc65655db6d29195ccdaa3cdd519c9
RamzRjs <60762747+RameesaRijas@users.noreply.github.com>
2021-10-29 17:05:10 -0400

    added custom hooks, empty , show, create

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,20 +3,43 @@ import "components/Appointment/styles.scss";
 import Header from "./Header";
 import Empty from "./Empty";
 import Show from "./Show";
+import useVisualMode from "hooks/useVisualMode";
+import Form from "./Form";
+
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+const CREATE = "CREATE";
 
 function Appointment(props) {
   const { interview, time } = props;
+  const { mode, transition, back } = useVisualMode(
+    interview ? SHOW : EMPTY
+  );
+
+  const interviewers = [{}];
+
   return (
         <article className="appointment">
           <Header time={time}/>
 
-        { interview ?
-          <Show student ={interview.student} interviewer= {interview.interviewer}/>
-          : <Empty />
-        }
-        
+
+          {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+          {mode === SHOW && (
+          <Show
+            student={interview.student}
+            interviewer={interview.interviewer}
+          />
+          )}
+          { mode === CREATE && (
+          <Form interviewers={interviewers}
+            onSave={() => console.log("Save")} onCancel={() => back()}/>
+          )}
+          
         </article>
   );
 }
 
 export default Appointment;
+
+{/* <Form student="Rameesa" interviewer = {3} interviewers = {interviewers} onSave={action("onSave")} onCancel={action("onCancel")}/>) */}
+  // .add("Create", () => <Form interviewers = {interviewers} onSave={action("onSave")} onCancel={action("onCancel")}/>)
