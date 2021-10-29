61a49102c0e61968931c7b04a4560016e2a03b5c
RamzRjs <60762747+RameesaRijas@users.noreply.github.com>
2021-10-29 17:03:00 -0400

    custom hooks

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,34 @@
+import { useState } from "react";
+
+function useVisualMode(initial) {
+
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]); 
+
+  const transition = (newMode, replace = false) => {
+    if (newMode !== mode) {
+      setMode(newMode);
+      (replace ? newMode = history[history.length - 1] : setHistory([...history, newMode]));
+    }
+    
+  }
+
+  const back = () => {
+
+    if (history.length > 1) {
+      history.pop();
+      setHistory([...history]);
+      setMode(history[history.length - 1]);
+
+      // let historyNew = [...history];
+      // historyNew.pop();
+      // console.log(history, 'Before', historyNew);
+      // setHistory([ ...historyNew ]);
+      // console.log(history, 'after', historyNew);
+      // setMode(history[history.length - 1]);
+    }
+  }
+
+  return { mode , transition, back};
+}
+export default useVisualMode;
