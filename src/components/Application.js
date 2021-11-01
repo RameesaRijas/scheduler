700a17a00d6b57a445c19ebf81d30dc605443f85
RamzRjs <60762747+RameesaRijas@users.noreply.github.com>
2021-11-01 15:29:52 -0400

    Create Edit & delete

--- src/components/Application.js
+++ src/components/Application.js
@@ -33,13 +33,47 @@ export default function Application() {
     })
   }, [])
 
+  const bookInterview = (id, interview) => {
+    return axios.put(`/api/appointments/${id}`, {interview})
+      .then(res => {
+        const appointment = {
+          ...state.appointments[id],
+          interview: { ...interview }
+        };
+    
+        const appointments = {
+          ...state.appointments,
+          [id]: appointment
+        };
+        setState({...state, appointments});
+      })
+  }
+
+  const cancelInterview = (id) => {
+
+  return axios.delete(`/api/appointments/${id}`)
+      .then(res => {
+    const appointment = {
+      ...state.appointments[id],
+      interview: null
+    }; 
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment
+    };
+    setState({...state, appointments});
+  })
+    
+  }
 
   const appointmentsData = dailyAppointments.map(item => {
     const interview = getInterview(state, item.interview);
     return ( <Appointment  
     key={item.id} 
-    {...item} interview={interview} interviewers={interviewersOfDay}/>)
+    {...item} interview={interview} interviewers={interviewersOfDay} bookInterview={bookInterview} cancelInterview={cancelInterview}/>)
   });
+  
+
   
 
   return (
